adf6f34238525e698f483aa931c4839bc0f89717
Triple <sebastien.andrys@gmail.com>
2019-06-26 19:06:34 +0200

    cleaned project to begin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Test from './components/Test'
-import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-      <Test nameGOT="John Snowy"/>
-      <Header />
-    </div>
+    <div className="App"></div>
   );
 }
 
