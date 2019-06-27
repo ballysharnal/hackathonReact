cab534c19aaf16f6eec13d1630d9f24f1651811b
Triple <sebastien.andrys@gmail.com>
2019-06-27 09:20:05 +0200

    validators almost done + form functional

--- src/components/UserManage/CreateAccount.js
+++ src/components/UserManage/CreateAccount.js
@@ -1,47 +1,72 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { ValidatorForm } from 'react-form-validator-core';
-import TextValidator from './TextValidator';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './CreateAccount.css'
+import { ValidatorForm } from 'react-form-validator-core'
+import TextValidator from './TextValidator'
+import confirm from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 class CreateAccount extends Component {
     
     state = {
         url: 'http://localhost:4242/users/create-profile',
         validatorConfirmPassword: true,
+        typePassword: 'password',
         pseudo: '',
         email: '',
         password: '',
+        confirmPassword: '',
         avatar: ''
     }
 
     onSubmit = e => {
         e.preventDefault();
         if (e.target.password.value === e.target.confirmPassword.value){
-            this.setState({validatorConfirmPassword: true});
-            axios.post(
-                this.state.url, {
-                    pseudo: e.target.pseudo.value,
-                    email: e.target.email.value,
-                    password: e.target.password.value,
-                    forgetPassword: "null",
-                    avatar: e.target.avatar.value,
-                    UserBase_id: 1
-                }
-            );
+            if (window.confirm(`Êtes-vous sûr de créer votre compte avec ces informations ?`)) {
+                this.setState({validatorConfirmPassword: true});
+                axios.post(
+                    this.state.url, {
+                        pseudo: e.target.pseudo.value,
+                        email: e.target.email.value,
+                        password: e.target.password.value,
+                        forgetPassword: "null",
+                        avatar: e.target.avatar.value,
+                        UserBase_id: 1
+                    }
+                    );
+                    this.setState({
+                        pseudo: '',
+                        email:'',
+                        password:'',
+                        confirmPassword: '',
+                        avatar:'',
+                    }
+                )
+            }
         } else {
             this.setState({validatorConfirmPassword: false});
         }
     }
 
     handleChange = e => {
-        const name = e.target.name
+        const name = e.target.name;
         this.setState({[name]: e.target.value});
+    }
+
+    showPassword = () => {
+        if (this.state.typePassword === 'password') {
+            return this.setState({typePassword: 'text'});
+        }
+        this.setState({typePassword: 'password'});
     }
 
     render() {
         return (
-            <div>
+            <div className="createAccountClass">
+                <h1>ATLANT'ATTACK</h1>
+                <h2>CREATE YOUR ACCOUNT</h2>
                 <ValidatorForm 
                     onSubmit={this.onSubmit}
                     ref="form"
@@ -77,15 +102,25 @@ class CreateAccount extends Component {
                             placeholder="Type your password"
                             onChange={this.handleChange}
                             name="password"
-                            type="password"
+                            type={this.state.typePassword}
                             value={this.state.password}
                             validators={['required', "minStringLength:7", "maxStringLength:16"]}
                             errorMessages={['Entrer un mot de passe', "Le mot de passe doit contenir au moins 7 caractères", "Le mot de passe ne doit pas excéder les 16 caractères"]}
                         />
+                        {
+                            this.state.typePassword === 'password' ?
+                            <FontAwesomeIcon icon={faEyeSlash} onClick={this.showPassword}/>
+                            :  <FontAwesomeIcon icon={faEye} onClick={this.showPassword}/>
+                        }
                     </label>
                     <label>
                         Confirm Password
-                        <input type="text" name="confirmPassword" placeholder="Confirm password"/>
+                        <input 
+                            type="text" 
+                            name="confirmPassword" 
+                            placeholder="Confirm password"
+                            value={this.state.confirmPassword}
+                        />
                         {
                             this.state.validatorConfirmPassword == false ? 
                             <p>
@@ -96,7 +131,14 @@ class CreateAccount extends Component {
                     </label>
                     <label>
                         Avatar
-                        <input type="text" name="avatar" placeholder="Avatar Url"/>
+                        <TextValidator
+                            onChange={this.handleChange}
+                            name="avatar"
+                            type="text"
+                            value={this.state.avatar}
+                            validators={['matchRegexp:^\.(gif|jpg|jpeg|tiff|png)']}
+                            errorMessages={['Only png, jpg, jpeg']}
+                        />
                     </label>
                     <button type="submit">CREATE ACCOUNT</button>
                 </ValidatorForm>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './HomePage.css';
 
 class HomePage extends Component {
     render() {
         return (
-            <div>
+            <div className="homePageClass">
                 <p>HomePage</p>
                 <Link to={"/Login"}>Login</Link>
                 <Link to={"/CreateAccount"}>CreateAccount</Link>
