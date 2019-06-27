a274ef8afc8a76fc140e99c364c3c47e1b5ae48c
Julia Hu <juliahukoshi@gmail.com>
2019-06-28 01:56:34 +0200

    soldier component in progress

--- src/components/BuySoldier.js
+++ src/components/BuySoldier.js
@@ -1,6 +1,8 @@
 import './BuySoldier.css'
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Redirect } from 'react-router-dom';
+
 
 class BuySoldier extends Component {
     state = {
@@ -12,10 +14,12 @@ class BuySoldier extends Component {
         soldatDefense: 0,
         soldatInitiative: 0,
         soldierUrl: 'http://localhost:4242/soldier/get-soldier',
+        userId: 0,
         userMoney: 0,
         userUrl: 'http://localhost:4242/users/get-user',
+        postMoneyUrl:'http://localhost:4242/users/update-user',
         soldierQuantity: 0,
-        operator: ''
+        priceToPay: 0
     };
 
     soldatInfos(){
@@ -32,40 +36,98 @@ class BuySoldier extends Component {
                 soldatDefense: res.data[0].soldatDefense,
                 soldatInitiative: res.data[0].soldatInitiative
             })
-            console.log(this.state.soldatName)
         }).catch(error =>{
+            console.log(error);
+        })
+    }
+
+    userInfos(){
+        axios({
+            method: 'Get',
+            url: this.state.userUrl,
+        }).then (res => {
+            this.setState({
+                userId: res.data[0].id,
+                userMoney: res.data[0].userMoney
+            })
+            console.log(this.state.userId)
+        }).catch(error => {
             console.log(error);
         })
     }
 
     componentDidMount(){
         this.soldatInfos();
+        this.userInfos();
     }
 
     quantitySoldiers = e => {
         e.preventDefault()
         let quantity = document.getElementsByTagName('input')[0].value;
+        let money = this.state.priceToPay;
+        let price = this.state.soldatPrice;
         if (e.target.innerText === '+') {
             quantity++
-            this.setState({soldierQuantity: quantity})
-        } else {
+            money += price
+            this.setState({
+                soldierQuantity: quantity,
+                priceToPay: money
+            })
+        } else if (e.target.innerText === '-' && quantity != 0){
             quantity--
-            this.setState({soldierQuantity: quantity})
+            money -= price
+            this.setState({
+                soldierQuantity: quantity,
+                priceToPay: money
+            })
+        } else {
+            quantity = quantity;
+        }
+    }
+
+    confirmSoldierBuying = e => {
+        e.preventDefault()
+        if (confirm ('Êtes vous sûr de vouloir acheter des soldats ?')) {//eslint-disable-line
+            let userMoney = this.state.userMoney - this.state.priceToPay
+            axios.put(
+                this.state.postMoneyUrl,
+                {
+                    id: this.state.userId,
+                    userMoney: userMoney
+                }
+            ).then(() => {
+                this.setState({ redirect: true });
+            });
+        } else {
+            alert('Transaction annulée !')
         }
     }
 
     render() {
+
+        const redirect = this.state.redirect;
+            if (redirect) {
+                return <Redirect to='/Game'/>;
+            }
+
         return (
-            <div>
+            <div className="main">
                 <h3>{this.state.soldatName}</h3>
-                <p>{this.state.soldatDescription}</p>
                 <img src={this.state.soldatImage} alt='Ryan' />
+                <p>{this.state.soldatDescription}</p>
+                <p>{this.state.soldatPrice}</p>
+                <p>{this.state.soldatAttackw}</p>
+                <p>{this.state.soldatDefense}</p>
+                <p>{'Initiative : ' + this.state.soldatInitiative}</p>
+                <p>{'Your money : ' + this.state.userMoney}</p>
                 <label>
                     Nombre de soldats souhaité :
-                    <button onClick={this.quantitySoldiers} type='button'>+</button>
+                    <button onClick={this.quantitySoldiers} type='button'>-</button>
                     <input type='number' value={this.state.soldierQuantity}/>
-                    <button type='button' onClick={this.quantitySoldiers}>-</button>
+                    <button type='button' onClick={this.quantitySoldiers}>+</button>
                 </label>
+                <p>{this.state.priceToPay}</p>
+                <button type='button' onClick={this.confirmSoldierBuying}>ACHETER</button>
             </div>
         )
     }
